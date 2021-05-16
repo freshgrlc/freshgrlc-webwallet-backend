e44a812b024c8990c9b71c157a7e38b97bda61f6
Erik van `t Wout <erik@ewnetworks.eu>
2021-05-16 16:13:05 +0200

    * Support for bech32 authentication

--- sigverify.py
+++ sigverify.py
@@ -4,11 +4,15 @@ from coinsupport.coins import GRLC
 
 from config import DAEMON_URL, DAEMON_ADDRESS_VERSION_BYTE
 
+COIN = GRLC
+
 
 def verify_signature(address, message, signature):
-    _, _, pubkeyhash = decode_any_address(address)
+    type, version, pubkeyhash = decode_any_address(address)
     address_for_verification = encode_base58_address(DAEMON_ADDRESS_VERSION_BYTE, pubkeyhash).decode('utf-8')
-    address_for_account_id = encode_base58_address(GRLC['address_version'], pubkeyhash).decode('utf-8')
+    if type != 'base58':
+        version = COIN['segwit_info']['address_version'] if 'address_version' in COIN['segwit_info'] else COIN['address_version']
+    address_for_account_id = encode_base58_address(version, pubkeyhash).decode('utf-8')
 
     if not Daemon(DAEMON_URL).verifymessage(address_for_verification, signature, message):
         return None
